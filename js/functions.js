4945e9d37f3d424e28c0c0a81c14ed9468323088
Jana Ma <janama@Janas-MacBook-Pro.local>
2018-09-26 19:02:39 -0700

    Changed to my own code

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,45 @@
 
-function sayHello(name) {
+function sayHello(Jana) {
   // TODO: return a greeting string that greets
   // the person by name, e.g. "Hello, Emily!"
+  var sayhello = 'Jana';
 }
 
-function sayHelloEverybody(threeNames) {
+function sayHelloEverybody(Jana Emily Maryclare) {
   // TODO: return a greeting string that greets
   // all three people in the threeNames array
   // e.g. "Hello, Emily, MaryClare, and Taurin!"
   // NOTE: inputs should be an array with exactly three items
+  var sayHelloEverybody = 'Jana Emily Maryclare'
 }
 
-function getClasses() {
+function getClasses(Art, Mathematics, and Dance) {
   // TODO: return an array with a list of strings
   // naming the classes you are taking this semester
+  var getClasses = 'Art, Mathematics, and Dance'
 }
 
-function labelOrder(fiveNumbers) {
+function labelOrder(22237) {
   // TODO: Given an array with five numbers, return a new array
   // of strings that have labels for the ordering.
   // e.g. if fiveNumbers is [1,2,3,4,5] your function should return:
   // ["first: 1", "second: 2", "third: 3", "fourth: 4", "fifth: 5"]
   // NOTE: inputs should be an array with exactly five items
+  var labelOrder = '22237'
 }
 
-function calculateVolume(w, l, h) {
+function calculateVolume(1, 2, 3) {
   // TODO: given a room's width, length, and height
   // write a function that returns the volume
+    var calculateVolume = '1, 2, 3'
 }
 
-function isOdd(n) {
+function isOdd(hereismyquestion) {
   // TODO: given a number, return true if it is odd, false if it is even
   // HINT: the expression "Boolean(0)" is false, "Boolean(1)"" is true
   // DOUBLE HINT: Try using the modulus operator (%)
-}
+    var isOdd = hereismyquestion)
+    {
 
 // ** CART PRICES ** //
 
@@ -45,17 +51,18 @@ function getSalePrice(originalPrice) {
   // (Don't worry about rounding to the correct number of decimal places)
 }
 
-function getSalePrices(threeOriginalPrices) {
+function getSalePrices(100 dollars) {
   // TODO: given an array of prices for three items in a cart,
   // return a new array with the saleAmount discount applied to each.
   // TIP: see if you can reuse the getSalePrice function that you just wrote!
   // NOTE: inputs should be an array with exactly three items
+  var result = getSalePrice ("100 dollars")
 }
 
  // ** COUNTING ** //
 
 var counter = 0;
-function incrementCounter() {
+function incrementCounter(123456) {
   // TODO: implement a function that increments (adds 1) to the counter variable
   // and then calls showCounter to update the interface with the new value.
   // Test your code by opening the developer console and calling incrementCounter.
