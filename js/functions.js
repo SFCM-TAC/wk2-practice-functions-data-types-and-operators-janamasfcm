9b5f0132c380aec1c06b8142852bc5973bcdf061
Jana Ma <janama@janas-mbp.attlocal.net>
2018-11-04 12:07:15 -0800

    11/4/18 Numbers 1 -8 are working

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,66 @@
 
-function sayHello(Jana) {
+function sayHello(name) {
   // TODO: return a greeting string that greets
   // the person by name, e.g. "Hello, Emily!"
-  var sayhello = 'Jana';
+  var name;
+  var greeting;
+  greeting = "Hi, " + name + "!";
+  return greeting;
 }
 
-function sayHelloEverybody(Jana Emily Maryclare) {
+function sayHelloEverybody(threeNames) {
   // TODO: return a greeting string that greets
   // all three people in the threeNames array
   // e.g. "Hello, Emily, MaryClare, and Taurin!"
   // NOTE: inputs should be an array with exactly three items
-  var sayHelloEverybody = 'Jana Emily Maryclare'
+  var nameList = ["Jana", " Emily", "Maryclare"]
+  nameList[2] = " and " + nameList[2];
+  var greeting = "Hello, " + nameList;
+  return greeting;
 }
 
-function getClasses(Art, Mathematics, and Dance) {
+function getClasses() {
   // TODO: return an array with a list of strings
   // naming the classes you are taking this semester
-  var getClasses = 'Art, Mathematics, and Dance'
+  return ["PVL", "Music", "Conducting", "compsci", " Game Audio."];
 }
 
-function labelOrder(22237) {
+function labelOrder(fiveNumbers) {
   // TODO: Given an array with five numbers, return a new array
   // of strings that have labels for the ordering.
   // e.g. if fiveNumbers is [1,2,3,4,5] your function should return:
   // ["first: 1", "second: 2", "third: 3", "fourth: 4", "fifth: 5"]
   // NOTE: inputs should be an array with exactly five items
-  var labelOrder = '22237'
-}
+  var numberList = [" 1", " 2", " 3", " 4", " 5"];
+  numberList[0] = " First:" + numberList[0];
+  numberList[1] = " Second:" + numberList[1];
+  numberList[2] = " Third:" + numberList[2];
+  numberList[3] = " Fourth:" + numberList[3];
+  numberList[4] = " Fiftht:" + numberList[4];
+  return numberList;
+  }
 
-function calculateVolume(1, 2, 3) {
+function calculateVolume(w, l, h) {
   // TODO: given a room's width, length, and height
   // write a function that returns the volume
-    var calculateVolume = '1, 2, 3'
+  var w = 3;
+  var l = 4;
+  var h = 2;
+  var volume = w * l * h;
+  return volume;
 }
 
-function isOdd(hereismyquestion) {
+function isOdd(n) {
   // TODO: given a number, return true if it is odd, false if it is even
   // HINT: the expression "Boolean(0)" is false, "Boolean(1)"" is true
   // DOUBLE HINT: Try using the modulus operator (%)
-    var isOdd = hereismyquestion)
-    {
+  var number = 3;
+  var remainder = number % 2;
+  if (remainder > 0){
+    return "it is odd";}else{
+    return "It is even";
+  }
+}
 
 // ** CART PRICES ** //
 
@@ -49,24 +70,30 @@ function getSalePrice(originalPrice) {
   // a price with the saleAmount discount applied
   // e.g. if originalPrice is 4.00, your function should return 3.00.
   // (Don't worry about rounding to the correct number of decimal places)
+  var salePrice = 5.99;
+  var discount = salePrice * saleAmount;
+  return discount;
 }
 
-function getSalePrices(100 dollars) {
+function getSalePrices(threeOriginalPrices) {
   // TODO: given an array of prices for three items in a cart,
   // return a new array with the saleAmount discount applied to each.
   // TIP: see if you can reuse the getSalePrice function that you just wrote!
   // NOTE: inputs should be an array with exactly three items
-  var result = getSalePrice ("100 dollars")
+  var priceList = [1.00, 2.00, 3.00];
+  priceList[0] = priceList[0] * saleAmount;
+  priceList[1] = priceList[1] * saleAmount;
+  priceList[2] = priceList[2] * saleAmount;
+  return priceList;
 }
 
  // ** COUNTING ** //
 
 var counter = 0;
-function incrementCounter(123456) {
-  // TODO: implement a function that increments (adds 1) to the counter variable
-  // and then calls showCounter to update the interface with the new value.
-  // Test your code by opening the developer console and calling incrementCounter.
-  // The displayed answer for this question should increase by 1!
+function incrementCounter() {
+  counter++;
+  showCounter();
+  return counter;
 }
 function showCounter() {
   // updates the counter element with the latest value
